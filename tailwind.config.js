fde94a3d0e58221ffd87fab38837b10083ee5cb3
Edward Ornelas <33706511+ornelasedward@users.noreply.github.com>
2022-08-16 18:47:30 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spin: "spin 20s linear infinite",
+      },
+    },
     screens: {
       "3xs": "150px",
 
