4b5e614f16ad62b6f84a05f4f396f3c98e544b41
Edward Ornelas <33706511+ornelasedward@users.noreply.github.com>
2022-09-12 12:13:56 -0500

    Add files via upload

--- components/Parallax.jsx
+++ components/Parallax.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/Image";
+import Image from "next/image";
 import Link from "next/link";
 import Bubble from "../assets/bubble3.png";
 import Bubble1 from "../assets/bubble1.png";
