9f269974280abf26c973710f9581c9445b8834f4
Edward Ornelas <33706511+ornelasedward@users.noreply.github.com>
2022-09-19 13:17:23 -0500

    Add files via upload

--- components/ServicesExplained.jsx
+++ components/ServicesExplained.jsx
@@ -99,7 +99,7 @@ const ServicesExplained = () => {
                   <div className="w-[1.125rem] h-[1.125rem] bg-white rounded-full border-2 border-[#1E1E1F] ml-1" />
                 </div>
 
-                <h4 className="text-2xl font-extrabold m-[2rem] text-center justify-center items-center">
+                <h4 className="text-2xl font-extrabold m-[1.8rem] text-center justify-center items-center">
                   Search Engine Optimization (seo)
                 </h4>
               </div>
